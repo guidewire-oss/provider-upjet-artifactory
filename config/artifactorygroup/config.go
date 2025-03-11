d2c48473042754f619b291e5c4e2617be47d5f17
Alex <alexchan2988@gmail.com>
2025-03-11 12:05:33 -0400

    add comment to resolve lint error
    Signed-off-by: Alex <alexchan2988@gmail.com>

--- config/artifactorygroup/config.go
+++ config/artifactorygroup/config.go
@@ -2,6 +2,7 @@ package artifactorygroup
 
 import "github.com/crossplane/upjet/pkg/config"
 
+// Configure the "artifactory_*" resources.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("artifactory_group", func(r *config.Resource) {
 		r.Kind = "ArtifactoryGroup"
--- config/artifactoryuser/config.go
+++ config/artifactoryuser/config.go
@@ -2,6 +2,7 @@ package artifactoryuser
 
 import "github.com/crossplane/upjet/pkg/config"
 
+// Configure the "artifactory_*" resources.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("artifactory_user", func(r *config.Resource) {
 		r.Kind = "ArtifactoryUser"
